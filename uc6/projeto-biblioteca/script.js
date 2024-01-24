e11d0e10d797fb926982e01e303550e9ea271846
DANUBIA BARBOSA CAMARGO <danubia.bcamargo@senacsp.edu.br>
2024-01-23 22:11:07 -0300

    herança ok

--- uc6/projeto-biblioteca/script.js
+++ uc6/projeto-biblioteca/script.js
@@ -5,12 +5,12 @@ class MaterialBibliografico {
         this.disponivel = true;
     }
 
-    realizarEmprestimo(){
-        if(this.disponivel == true){
+    realizarEmprestimo() {
+        if (this.disponivel) {
             this.disponivel = false;
             return true; //Emprestimo realizado com sucesso
-        }else{
-            return false; // Material já emprestado
+        } else {
+            return false; //Material já emprestado
         }
     }
 
@@ -43,8 +43,29 @@ class Revista extends MaterialBibliografico {
 
 }
 
-function realizarAcao(acao){
-    
+function realizarAcao(acao) {
+    const selectLivros = document.getElementById("livros");
+    const selectedIndex = selectLivros.selectedIndex;
+
+    if(selectedIndex === 0) {
+        alert("Por favor, selecione um livro válido.");
+        return;
+    }
+
+    const livroSelecionado = livros[selectedIndex - 1];
+
+    if(acao === 'emprestimo') {
+        const emprestimoSucesso = livroSelecionado.realizarEmprestimo();
+        exibirResultado(`Emprestimo de ${livroSelecionado.titulo}: ${emprestimoSucesso? 'Sucesso' : 'Material já emprestado'}`);
+    } else if (acao === 'devolucao') {
+        const devolucaoSucesso = livroSelecionado.realizarDevolucao();
+        exibirResultado(`Devolução de ${livroSelecionado.titulo}: ${devolucaoSucesso? 'Sucesso' : 'Material já devolvido'}`);
+    }
+}
+
+function exibirResultado(mensagem){
+    const resultadoDiv = document.getElementById("resultado");
+    resultadoDiv.innerHTML += `<p>${mensagem}</p>`
 }
 
 const livros = [
