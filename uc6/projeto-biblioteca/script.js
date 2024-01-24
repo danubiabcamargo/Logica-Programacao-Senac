cbfc79a564d68a30832d42c8dcd600e5bc452e1f
DANUBIA BARBOSA CAMARGO <danubia.bcamargo@senacsp.edu.br>
2024-01-23 21:42:41 -0300

    tela criada

--- uc6/projeto-biblioteca/script.js
+++ uc6/projeto-biblioteca/script.js
@@ -42,3 +42,27 @@ class Revista extends MaterialBibliografico {
     }
 
 }
+
+function realizarAcao(acao){
+    
+}
+
+const livros = [
+    new Livro ("Dom Casmurro - ","Machado de Assis", "Realismo"),
+    new Livro ("Cem Anos de Solidão - "," Gabriel García Márquez", "Realismo Mágico"),
+    new Livro ("1984","George Orwell - ", "Ficção Distópica"),
+    new Livro ("O Senhor dos Anéis - "," J.R.R. Tolkien", "Fantasia"),
+    new Revista("National Geographic -", " Ciência e Natureza"),
+
+];
+
+const selectLivros = document.getElementById("livros");
+
+for(let i = 0; i < livros.length; i++){
+    const livro = livros [i];
+    const option = document.createElement("option");
+    option.value = i + 1; //add 1 para evitar o valor 0, que representa a opç padrão
+    option.text = livro.titulo + livro.autor;
+    selectLivros.add(option);
+
+}
